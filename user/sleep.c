d3cbf2005bb84f645e1c4bb392845b85f7895f64
Yu-mi-kuang <2446745341@qq.com>
2024-03-18 17:09:12 +0800

    lab1 sleep

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc != 2) {
+    fprintf(2, "Invalid parameter\n");
+    exit(1);
+  }
+
+  int time;
+  time = atoi(argv[1]);
+  sleep(time);
+  exit(0);
+}
