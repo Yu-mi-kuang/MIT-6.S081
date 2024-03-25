531e9edb03d7013ccebd0226ec15019b4ae5ef71
Yu-mi-kuang <2446745341@qq.com>
2024-03-25 17:02:47 +0800

    upload lab1 sleep

--- user/sleep.c
+++ user/sleep.c
@@ -2,16 +2,14 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int
-main(int argc, char *argv[])
-{
-  if(argc != 2) {
-    fprintf(2, "Invalid parameter\n");
-    exit(1);
-  }
+int main(int argc, char *argv[]) {
+    if(argc != 2) {
+        fprintf(2, "Invalid parameter\n");
+        exit(1);
+    }
 
-  int time;
-  time = atoi(argv[1]);
-  sleep(time);
-  exit(0);
+    int time;
+    time = atoi(argv[1]);
+    sleep(time);
+    exit(0);
 }
